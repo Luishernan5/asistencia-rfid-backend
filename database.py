b51c4f79dc5ae82d09526b3553ee45dad76f6958
Luishernan5 <ramirezluishernan5@gmail.com>
2025-06-17 16:24:12 -0600

    cambio 4 main

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import os
 
-DB_USER = os.getenv('DB_USER', 'origaska_origasca')
+DB_USER = os.getenv('DB_USER', 'origaska_origaska')
 DB_PASS = os.getenv('DB_PASSWORD', 'K(0u9co4G0n)BT')
 DB_HOST = os.getenv('DB_HOST', '67.217.36.136')
-DB_NAME = os.getenv('DB_NAME', 'Origasca-rfid')
+DB_NAME = os.getenv('DB_NAME', 'Origaska-rfid')
 
 DATABASE_URL = f"mysql+asyncmy://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
 
